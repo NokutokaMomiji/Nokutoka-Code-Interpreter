6000a3d839fd10efe6bdd9c1203c8d352c977c91
Emilia Peñaloza <58234390+NokutokaMomiji@users.noreply.github.com>
2022-10-07 13:12:20 +0200

    Working on Compiler. Computer running out of battery.
    THis is what happens when you forget to charge the school laptop during nighttime.

--- src/compiler.c
+++ src/compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
@@ -7,11 +8,22 @@ typedef struct {
     Token Current;
     Token Previous;
     bool hadError;
+    bool panicMode;
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* CurrentChunk() {
+    return compilingChunk;
+}
 
 static void ErrorAt(Token* token, const char* msg) {
+    if (parser.panicMode)
+        return;
+
+    parser.panicMode = true;
+
     fprintf(stderr, "[ERROR]: %s ", msg);
 
     if (token->Type == TOKEN_EOF)
@@ -36,6 +48,10 @@ static void CompilerAdvance() {
 
     for (;;) {
         parser.Current = ScannerScanToken();
+
+        if (parser.Current.Line != parser.Previous.Line)
+            ChunkIncreaseLine(CurrentChunk());
+
         if (parser.Current.Type != TOKEN_ERROR)
             break;
 
@@ -43,9 +59,43 @@ static void CompilerAdvance() {
     }
 }
 
+static void CompilerConsume(TokenType type, const char* msg) {
+    if (parser.Current.Type == type) {
+        CompilerAdvance();
+        return;
+    }
+
+    ErrorAtCurrent(msg);
+}
+
+static void CompilerEmitByte(uint8_t byte) {
+    ChunkWrite(CurrentChunk(), byte);
+}
+
+static void CompilerEmitBytes(uint8_t firstByte, uint8_t secondByte) {
+    CompilerEmitByte(firstByte);
+    CompilerEmitByte(secondByte);
+}
+
+static void CompilerEmitLong(long longNumber) {
+    ChunkWriteLong(CurrentChunk(), longNumber);
+}
+
+static void CompilerEmitByteLong(uint8_t byte, long longNumber) {
+    CompilerEmitByte(byte);
+    CompilerEmitLong(longNumber);
+}
+
 bool Compile(const char* source, Chunk* chunk) {
     ScannerInit(source);
+
+    compilingChunk = chunk;
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
     ScannerAdvance();
     ScannerExpression();
     ScannerConsume(TOKEN_EOF, "Expected end of expression");
+    return !parser.hadError;
 }
